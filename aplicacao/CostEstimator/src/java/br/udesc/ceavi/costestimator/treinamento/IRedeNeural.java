b2b8b66f6485fc40915e10feeda902964546542a
Diego Heusser <diego.heusser@outlook.com>
2015-06-16 16:45:11 -0300

    Criado as classes da MLP

---
+++ aplicacao/CostEstimator/src/java/br/udesc/ceavi/costestimator/treinamento/IRedeNeural.java
@@ -0,0 +1,15 @@
+package br.udesc.ceavi.costestimator.treinamento;
+
+/**
+ *
+ * @author Diego Heusser
+ */
+public interface IRedeNeural {
+    
+    public int treinamento();
+    
+    public double[] predicao(double[] dados);
+    
+    public void close();
+    
+}
